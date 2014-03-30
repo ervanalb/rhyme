7c300c5bb5784778f15bef453c7c30f59f93735a
Eric Van Albert <eric@ericslaptop.(none)>
2014-03-30 04:50:48 -0400

    image comes from HTTP now instead of webcam, for use with ipwebcam

--- camera.py
+++ camera.py
@@ -1,23 +1,45 @@
 #!/usr/bin/python
 
 import pygame
-import pygame.camera
 from pygame.locals import *
+import pygame.camera
+
+import urllib2
+import StringIO
 
 import blob
 import colorsys
 from PIL import Image
+import ImageFilter
 
 class Camera:
 	def __init__(self,size=(640,480)):
 		self.size=size
 		pygame.init()
+		self.display = pygame.display.set_mode(self.size, 0)
+
+	def pygame_webcam_init(self):
 		pygame.camera.init()
 		self.cam=pygame.camera.Camera("/dev/video0",self.size)
 		self.cam.start()
-		self.display = pygame.display.set_mode(self.size, 0)
+
+	def web_frame_init(self,url):
+		self.url=url
 
 	def get_frame(self):
+		print "fetch..."
+		s=urllib2.urlopen(self.url).read()
+		im = Image.open(StringIO.StringIO(s))
+		print "resize..."
+		im = im.resize(self.size,Image.ANTIALIAS)
+		im=im.filter(ImageFilter.BLUR)
+		letter_array=im.load()
+
+		letter_array=[[(float(letter_array[x,y][0])/255.,float(letter_array[x,y][1])/255.,float(letter_array[x,y][2])/255.) for y in range(im.size[1])] for x in range(im.size[0])]
+		print "process..."
+		return letter_array
+
+	def get_frame_webcam(self):
 		s=self.cam.get_image()
 		pa=pygame.PixelArray(s)
 		def conv(p):
@@ -46,14 +68,14 @@ class Camera:
 			events = pygame.event.get()
 			for e in events:
 				if e.type == QUIT or (e.type == KEYDOWN and e.key == K_ESCAPE):
-					self.cam.stop()
+					#self.cam.stop()
 					going = False
 
 	def process(self):
 		p=self.get_frame()
 		hsv=blob.convert(p,lambda x:colorsys.rgb_to_hsv(*x))
 		def thresh((h,s,v)):
-			return abs(h-194./360.) < 30./360. and s > 10./100. and s < 80./100. and v > 2./100.
+			return abs(h-176./360.) < 30./360. and s > 50./100.# and v > 10./100.
 
 		blobs=blob.find_all(hsv,thresh)
 		for b in blobs:
@@ -65,6 +87,7 @@ class Camera:
 
 if __name__=='__main__':
 	c=Camera()
+	c.web_frame_init('http://10.0.0.16:8080/photoaf.jpg')
 	im=Image.open('letter_imgs/test.png')
 	w,h=im.size
 	ratio=4
